22e4525c153eaa543e267694dc3379e984d9db9f
Nikki-boop <bogdnico@gmail>
2024-10-14 15:40:41 -0500

    Assignment 1

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import './styles/navbar.css';
+
+function Navbar(){
+    return(
+        <div>
+            <nav>
+                <div>
+                    <a href=''>Log In</a>
+                    <a href=''>Search</a>
+                    <a href=''>Subscribe</a>
+                    <a href=''>Menu</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
