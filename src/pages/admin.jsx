3762401e10949cd97c63108f3f80f325e49a71d7
Nikki-boop <bogdnico@gmail>
2024-10-28 11:08:37 -0500

    assign.3

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './styles/admin.css'
 
 function Admin (){
 
+    const [coupon, setCoupon] = useState({
+        code: '',
+        discount: ''
+    });
+
     function handleCouponInput(e){
-        console.log("val changed")
+        const val =  e.target.value;
+        const name = e.target.name;
+
+        console.log(name, val);
+
+        // if (name === "code") {
+            
+        // }
     }
+
 
     return(
         <div className="admin page">
@@ -15,19 +29,19 @@ function Admin (){
                     <h4>Manage Products</h4>
                     <div>
                         <label className='form-label'>Title</label>
-                        <input className='form-control'></input>
+                        <input className='form-control' name="title" onChange={handleCouponInput}></input>
                     </div>
                     <div>
                         <label className='form-label'>Category</label>
-                        <input className='form-control'></input>
+                        <input className='form-control' name="category" onChange={handleCouponInput}></input>
                     </div>
                     <div>
                         <label className='form-label'>Image</label>
-                        <input className='form-control'></input>
+                        <input className='form-control' name="image" onChange={handleCouponInput}></input>
                     </div>
                     <div>
                         <label className='form-label'>Price</label>
-                        <input className='form-control'></input>
+                        <input className='form-control' name="price" onChange={handleCouponInput}></input>
                     </div>
                     <div className="controls">
                         <button className='btn btn-outline-dark'>Save Product</button>
@@ -39,11 +53,11 @@ function Admin (){
 
                     <div>
                         <label className='form-label'>Code</label>
-                        <input className='form-control' onChange={handleCouponInput}></input>
+                        <input className='form-control' name="code" onChange={handleCouponInput}></input>
                     </div>
                     <div>
                         <label className='form-label'>Discount</label>
-                        <input className='form-control'></input>
+                        <input className='form-control' type='number' name="discount" onChange={handleCouponInput}></input>
                     </div>
 
                     <div className="controls">
